05b20f89886d52abab95cc086f98a71e947b548f
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-24 16:29:47 +0900

    서버 가동 확인

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+
+const contentType = {
+  'content-Type' : 'text/html',
+  'charset' : 'utf-8',
+}
+
+const server = http.createServer((req, res) => {
+  if(req.method === 'GET' && req.url === '/') {
+    fs.readFile('./Travel-Website.html' ,(err,data) => {
+      if(err) {
+        console.error('파일 호출 에러');
+      } else {
+        res.writeHead(200, contentType);
+        res.end(data);
+      }
+    })
+  }
+});
+server.listen(8080, function(){
+  console.log('서버가 가동중입니다. 끄려면 Cstrl + c 를 누르세요');
+});
