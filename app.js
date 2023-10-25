46f6d6eac4e6871f8d47d5bafacb61cc0e1b5bb0
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-25 16:25:45 +0900

    서버 다시 작성

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
 const http = require('http');
 const fs = require('fs');
-
-const contentType = {
-  'content-Type' : 'text/html',
-  'charset' : 'utf-8',
-}
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  if(req.method === 'GET' && req.url === '/') {
-    fs.readFile('./Travel-Website.html' ,(err,data) => {
-      if(err) {
-        console.error('파일 호출 에러');
-      } else {
-        res.writeHead(200, contentType);
-        res.end(data);
-      }
-    })
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      // HTML 파일 제공
+      fs.readFile('./Travel-Website.html', 'utf-8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('서버 오류');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    } else if (req.url === '/main.css') {
+      // CSS 파일 제공
+      fs.readFile('./main.css', 'utf-8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('서버 오류');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/css' });
+          res.end(data);
+        }
+      });
+    } else if (req.url === '/script.js') {
+      // JavaScript 파일 제공
+      fs.readFile('./script.js', 'utf-8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('서버 오류');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/javascript' });
+          res.end(data);
+        }
+      });
+    } else if (req.url.startsWith('/image/')) {
+      // 이미지 파일 제공
+      const imagePath = path.join(__dirname, req.url);
+      fs.readFile(imagePath, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('이미지를 찾을 수 없습니다.');
+        } else {
+          const ext = path.extname(imagePath).toLowerCase();
+          let contentType = 'image/jpeg'; // 기본 이미지 유형
+          if (ext === '.png') {
+            contentType = 'image/png';
+          } else if (ext === '.gif') {
+            contentType = 'image/gif';
+          }
+          res.writeHead(200, { 'Content-Type': contentType });
+          res.end(data);
+        }
+      });
+    } else {
+      // 정의되지 않은 URL에 대한 처리
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('페이지를 찾을 수 없습니다.');
+    }
   }
 });
-server.listen(8080, function(){
-  console.log('서버가 가동중입니다. 끄려면 Cstrl + c 를 누르세요');
+
+server.listen(8080, () => {
+  console.log('서버가 가동중입니다. 끄려면 Ctrl + C를 누르세요.');
 });
