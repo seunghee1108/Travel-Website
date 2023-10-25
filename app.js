dfdeb749eec998358c68cc2b65175a1d3fee1561
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-26 00:53:12 +0900

    주석 작성

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+// 내장모듈인 path 모듈을 사용하겠다는 것
 const path = require('path');
 
+// 서버 생성
 const server = http.createServer((req, res) => {
+  // 이 두 경우 모두 HTML 파일을 제공할 것이다.
   if (req.url === '/' || req.url === '/Travel-Website.html') {
+    // __dirname : 현재 스크립트 파일이 위치한 디렉토리를 타나낸다.
+    // path.join() 함수를 사용하여 현재 디렉토리와 "Travel-Website.html"파일 이름을 결합하여 해당 HTML파일의 전체 경로를 얻습니다. 
     const filePath = path.join(__dirname, 'Travel-Website.html');
+    // 함수를 호출하여 파일을 클라이언트에게 보낸다.
+    // 콘텐츠 타입 : 'text/html'
     sendFile(res, filePath, 'text/html');
+    // 정리하면 클라이언트의 요청 URL이 "/"" 또는 "Travel-Website.html"인 경우에만 "Travel-Website.html"파일의 내용을 응답으로 전송한다. 
   } else if (req.url === '/main.css') {
     const filePath = path.join(__dirname, 'main.css');
     sendFile(res, filePath, 'text/css');
@@ -35,19 +43,28 @@ const server = http.createServer((req, res) => {
     const cssPath = path.join(__dirname, 'region', 'sub.css')
     sendFile(res, cssPath, 'text/css');
   } else {
+    // 404 에러
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('페이지를 찾을 수 없음');
   } 
 });
 
-
+// 3개의 매개변수
+// response 응답, filePath 전송할 파일의 경로, contentType 파일의 콘텐츠 타입
 function sendFile(res, filePath, contentType) {
+  // fs 모듈을 사용하여 파일을 비동기적으로 읽는다.
+  // filePath에 지정된 경로의 파일을 읽고, 읽은 데이터는 'data'변수에 저장
   fs.readFile(filePath, (err, data) => {
+    // 파일 일기 도중 오류 발생
+    // 500 서버 오류를 나타낸다. 
     if (err) {
       res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end('서버 에러');
+      // 오류가 없을 경우 
+      // 콘텐츠 타입 사용하여 200 성공
     } else {
       res.writeHead(200, { 'Content-Type': contentType });
+      // 응답으로 'data' 전송
       res.end(data);
     }
   });
--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // 슬라이드 제작
+// 슬라이더 컨테이너 변수
+// slider-container를 통해 html문서에 해당 클래스를 가진 요소를 찾아서 변수에 저장
 const sliderContainer = document.querySelector('.slider-container');
+// 슬라이더 변수 
+// 모든 슬라이더를 나타내는 요소들을 나타낸다.
 const sliders = document.querySelectorAll('.slider');
-// 전체 슬라이드 변수 선언
+// 슬라이드의 총 개수를 나타내는 변수 
 const totalSlides = sliders.length;
+// 현재 표시 중인 슬랄이드의 인덱스를 나타낸다. 초기값은 0임
 let currentIndex = 0;
 let timer; // 타이머 변수
 
+// showSlide() :  특정 인덱스의 슬라이드를 표시하는 역할을 함
+// 매개변수는 표시하려는 슬라이드의 인덱스를 나타낸다.
 function showSlide(index) {
-  // 
   // 0보다 작은 경우
+  // 마지막 슬라이드로 이동하도록 처리된다. 
 if (index < 0) {
-  //
 index = totalSlides - 1;
+// index가 슬라이드 총 개수보다 큰 경우,
+// 첫번째 슬라이드로 이동하도록 처리
 } else if (index >= totalSlides) {
 index = 0;
 }
@@ -23,34 +31,24 @@ slider.style.display = 'none';
 
 // 현재 인덱스의 슬라이드만 표시
 sliders[index].style.display = 'block';
+//
 currentIndex = index;
 
 // 이전 타이머를 클리어하고 3초 뒤에 다음 슬라이드로 이동하는 타이머 시작
 clearTimeout(timer);
+// nextSlide() : 다음 슬라이드로 이동하는 역할
 timer = setTimeout(nextSlide, 3000);
 }
 
 function nextSlide() {
+  // 호출하여 currentIndex
+  // showSlide함수에 현재 인덱스에서 1을 더한 값을 전달
+  // currentIndex : 현재 표시 중인 슬라이드의 인덱스를 나타낸다.
 showSlide(currentIndex + 1);
 }
-
+// preSlide() : 이전 슬라이드로 이동하는 역할
 function prevSlide() {
+  // 호출하여 
+  // slide 함수에 현재 인덱스에서 1을 뺀 값을 전달한다.
 showSlide(currentIndex - 1);
 }
-
-// 초기로드 시 첫 번째 슬라이드 표시
-showSlide(currentIndex);
-
-// 이전 및 다음 버튼 이벤트 처리
-// const prevBtn = document.getElementById('prevBtn');
-// const nextBtn = document.getElementById('nextBtn');
-
-// prevBtn.addEventListener('click', () => {
-// showSlide(currentIndex - 1);
-// clearTimeout(timer); // 버튼 클릭 시 타이머 클리어
-// });
-
-// nextBtn.addEventListener('click', () => {
-// showSlide(currentIndex + 1);
-// clearTimeout(timer); // 버튼 클릭 시 타이머 클리어
-// });
